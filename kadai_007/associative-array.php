0c7b105c312de29be263c85bd5fb8665ead932cd
mame802493 <147903218+mame802493@users.noreply.github.com>
2023-12-07 23:20:05 +0900

    kadai007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+<body>
+  <p>
+    <?php
+
+    $goods_date=["name"=>"onion","price"=>200,"weight"=>160];
+
+    print_r($goods_date);
+
+    ?>
+   </p>
+  
+</body>
+</html>
